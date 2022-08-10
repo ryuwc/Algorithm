9dece6d577bd3cb1c7299b7bf56f9a85d6781531
ryuwc <wom05169@gmail.com>
2022-08-10 22:55:26 +0900

    Create 2817_부분 수열의 합.py

---
+++ swea/2817_부분 수열의 합.py
@@ -0,0 +1,18 @@
+import sys; sys.stdin = open('input_부분 수열의 합.txt')
+
+T = int(input())
+for tc in range(1, T+1):
+    N, K = map(int, input().split())
+    numbers = list(map(int, input().split()))
+
+    counts = 0
+    for i in range(1<<N):
+        summary = 0
+        for j in range(N):
+            if i & (1<<j):
+                summary += numbers[j]
+        if summary == K:
+            counts += 1
+    print(f'#{tc} {counts}')
+
+
