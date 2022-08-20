3c94236b587a912ad98e42884129450cfaa292f4
ryuwc <wom05169@gmail.com>
2022-08-20 17:00:32 +0900

    none

--- swea/1979_어디에 단어가 들어갈 수 있을까.py
+++ swea/1979_어디에 단어가 들어갈 수 있을까.py
@@ -1,37 +1,43 @@
-# import sys; sys.stdin = open('input_어디.txt', 'r')
+import sys; sys.stdin = open('input_어디단어.txt', 'r')
 
-T = int(input())
-for tc in range(1, T+1):
+for tc in range(int(input())):
     N, K = map(int, input().split())
-    arr = [list(map(int, input().split())) for _ in range(N)]
-    counts_r = 0
-    counts_c = 0
+    arr = []
+    for _ in range(N):
+        arr += [list(map(int, input().split()))]
+    result = 0
+
+    # 행 검색
+    # result에 넣어줄 경우는 더하다가 마지막인덱스가 1이며 그 길이가 K인 경우,
+    # 그리고, 더하다가 0을 만났는데, 더했던게 K인 경우이다.
     for i in range(N):
-        cts = 0
+        sums = 0
         for j in range(N):
             if j == N-1 and arr[i][j] == 1:
-                cts += 1
-                if cts == K:
-                    counts_r += 1
+                sums += 1
+                if sums == K:
+                    result += 1
             elif arr[i][j] == 1:
-                cts += 1
-            elif arr[i][j] == 0 and cts ==K:
-                counts_r += 1
-                cts = 0
+                sums += 1
+            elif arr[i][j] == 0 and sums == K:
+                sums = 0
+                result += 1
             else:
-                cts = 0
+                sums = 0
+
+    # 열 검색
     for j in range(N):
-        cts = 0
+        sums = 0
         for i in range(N):
-            if i == N - 1 and arr[i][j] == 1:
-                cts += 1
-                if cts == K:
-                    counts_c += 1
+            if i == N-1 and arr[i][j] == 1:
+                sums += 1
+                if sums == K:
+                    result += 1
             elif arr[i][j] == 1:
-                cts += 1
-            elif arr[i][j] == 0 and cts == K:
-                counts_c += 1
-                cts = 0
+                sums += 1
+            elif arr[i][j] == 0 and sums == K:
+                sums = 0
+                result += 1
             else:
-                cts = 0
-    print(f'#{tc} {counts_r+counts_c}')
+                sums = 0
+    print(f'#{tc+1}', result)
