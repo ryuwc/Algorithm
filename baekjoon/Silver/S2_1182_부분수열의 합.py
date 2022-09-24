ef11c022aad86961c0603efe6436661b40573588
ryuwc <wom05169@gmail.com>
2022-09-24 22:56:25 +0900

    Update S2_1182_부분수열의 합.py

--- baekjoon/Silver/S2_1182_부분수열의 합.py
+++ baekjoon/Silver/S2_1182_부분수열의 합.py
@@ -3,6 +3,7 @@ N, S = map(int, input().split())
 arr = list(map(int, input().split()))
 
 result = []
+# dfs를 사용하여 모든 부분 수열을 만들어서 result에 저장
 def dfs(index, path):
     global result
     result.append(path)
@@ -14,6 +15,7 @@ dfs(0, [])
 
 ans = 0
 for i in range(len(result)):
+    # 부분 수열이 []으로 빈 리스트도 포함되어서 sum([])은 0이 되어버림 그래서 조건 추가
     if len(result[i])>0 and sum(result[i]) == S:
         ans += 1
 
