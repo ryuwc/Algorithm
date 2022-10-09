73f7ac4a2fa163fc99e5842cd4799f5b84d564d8
ryuwc <wom05169@gmail.com>
2022-10-09 16:02:57 +0900

    Update 684_Redundant Connection.py

--- leetcode/684_Redundant Connection.py
+++ leetcode/684_Redundant Connection.py
@@ -1,20 +1,23 @@
-def findRedundantConnection(self, edges: List[List[int]]) -> List[int]:
-    n = len(edges)
-    par = [i for i in range(n + 1)]
+class Solution(object):
+    def findRedundantConnection(self, edges):
 
-    def find(n):
-        p = par[n]
-        while par[p] != p:
-            p = par[p]
-        return p
+        n = len(edges)
+        # 부모 초기화
+        par = [i for i in range(n + 1)]
 
-    def union(n1, n2):
-        p1, p2 = find(n1), find(n2)
-        if p1 == p2:
-            return False
-        par[p2] = p1
-        return True
+        def find(n):
+            while par[n] != n:
+                n = par[n]
+            return n
+        # n1과 n2의 부모가 같다면 합치지 않고 False를 return (부모가 같은 노드를 합치면 싸이클이 형성된다)
+        def union(n1, n2):
+            p1, p2 = find(n1), find(n2)
+            if p1 == p2:
+                return False
+            par[p2] = p1
+            return True
 
-    for a, b in edges:
-        if not union(a, b):
-            return [a, b]
+        # 부모가 같은 노드를 발견 == 싸이클이 되므로 False가 return됨
+        for a, b in edges:
+            if not union(a, b):
+                return [a, b]
