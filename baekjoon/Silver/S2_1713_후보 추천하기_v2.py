17355c435615a098c3e4eda82a41729205a9e580
ryuwc <wom05169@gmail.com>
2023-06-11 21:11:15 +0900

    Create S2_1713_후보 추천하기_v2.py

---
+++ baekjoon/Silver/S2_1713_후보 추천하기_v2.py
@@ -0,0 +1,13 @@
+import sys
+
+
+
+def main():
+    input = sys.stdin.readline
+    N = int(input())
+    M = int(input())
+    candidates = list(map(int, input().split()))
+
+if __name__ == '__main__':
+    main()
+
