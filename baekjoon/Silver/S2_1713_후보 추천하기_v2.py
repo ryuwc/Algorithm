90ad9a4d979aed9ff2accd38809913a3c4fc7264
ryuwc <wom05169@gmail.com>
2023-06-12 22:05:30 +0900

    Update S2_1713_후보 추천하기_v2.py

--- baekjoon/Silver/S2_1713_후보 추천하기_v2.py
+++ baekjoon/Silver/S2_1713_후보 추천하기_v2.py
@@ -1,13 +1,30 @@
 import sys
-
+from collections import deque
 
 
 def main():
-    input = sys.stdin.readline
-    N = int(input())
-    M = int(input())
-    candidates = list(map(int, input().split()))
+    sinput = sys.stdin.readline
+    N = int(sinput())
+    M = int(sinput())
+    candidates = list(map(int, sinput().split()))
+
+    # [학생번호, 추천횟수, 인덱스]
+    rst = deque(maxlen=N)
+
+    for i, val in enumerate(candidates):
+        for info in rst:
+            if info[0] == val:
+                info[1] += 1
+                break
+        else:
+            if len(rst) == N:
+                rst = deque(sorted(list(rst), key=lambda x: (x[1], x[2])), maxlen=N)
+            rst.append([val, 1, i])
+
+    rst = [val[0] for val in rst]
+
+    print(*sorted(rst))
+
 
 if __name__ == '__main__':
     main()
-
