0bf7e3e4d5b46a993ae939b035093df19a78262e
ryuwc <wom05169@gmail.com>
2023-08-04 23:14:03 +0900

    Create B1_2775_부녀회장이 될테야.py

---
+++ baekjoon/Bronze/B1_2775_부녀회장이 될테야.py
@@ -0,0 +1,9 @@
+T = int(input())
+for tc in range(T):
+    k = int(input())
+    n = int(input())
+    people = [i for i in range(1, n + 1)]
+    for _ in range(k):
+        for i in range(1, n):
+            people[i] += people[i - 1]
+    print(people[-1])
